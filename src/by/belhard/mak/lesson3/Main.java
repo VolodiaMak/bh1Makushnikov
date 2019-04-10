48fe8c595aab1a4695f08cc11b7d4a1f6dd66681
volodiamak <volodiamak@mail.ru>
2019-04-10 20:22:21 +0300

    init

--- src/by/belhard/mak/lesson3/Main.java
+++ src/by/belhard/mak/lesson3/Main.java
@@ -1,4 +1,15 @@
 package by.belhard.mak.lesson3;
 
 public class Main {
+    public static void main(String[] args) {
+
+        String [] array = new String [3];
+
+        array [0] = "value1";
+        array [2] = "value3";
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+    }
+
 }
